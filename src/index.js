357484687d94be79abf47056c5121545d566d2ee
Alex <alexaumh@gmail.com>
2021-10-25 17:49:17 +1100

    Create API to retrieve RSS feed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const feedRoutes = require('./routes/feedRoutes');
+
+app.use('/', feedRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
